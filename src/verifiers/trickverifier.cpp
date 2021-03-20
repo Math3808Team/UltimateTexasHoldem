ef911de541d9da2592a84791bea28c6f4e8c1e28
Joshua Challenger <joshua.challenger@wphotonics.com>
2021-03-20 15:13:47 -0400

    fix no return value

--- src/verifiers/trickverifier.cpp
+++ src/verifiers/trickverifier.cpp
@@ -3,7 +3,8 @@
 
 QHash<int,int> TrickVerifier::getTableOfValues(Hand &hand) {
     hand.getCards();
+    return QHash<int, int>();
 }
 QHash<int,int> TrickVerifier::getTableOfSuits(Hand &hand) {
-
+    return QHash<int, int>();
 }
