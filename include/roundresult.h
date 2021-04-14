e5f1da7af31736997cdccd0835214f0c83010293
Joshua Challenger <joshua.challenger@wphotonics.com>
2021-04-14 11:33:48 -0400

    qstring conversion for testing

--- include/roundresult.h
+++ include/roundresult.h
@@ -10,7 +10,19 @@ struct RoundResult {
     int playPayout;
     int tripsPayout;
     int totalPayout;
+    operator QString() const;
 
 };
+
+RoundResult::operator QString() const {
+    QString ret;
+    ret.append("winner: " + QString::number(winner) + "\n");
+    ret.append("antePayout: " + QString::number(antePayout) + "\n");
+    ret.append("blindPayout: " + QString::number(blindPayout) + "\n");
+    ret.append("playPayout: " + QString::number(playPayout) + "\n");
+    ret.append("tripsPayout: " + QString::number(tripsPayout) + "\n");
+    ret.append("totalPayout: " + QString::number(totalPayout) + "\n");
+    return ret;
+}
 
 #endif // ROUNDRESULT_H
